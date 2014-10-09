e9e2027586425f60925dea642347ae3254906667
Ricardo Gladwell <ricardo@gladwell.me>
2014-10-09 12:13:39 +0100

    Configure API keys for Measurements API.

--- manifests/measures.pp
+++ manifests/measures.pp
@@ -58,17 +58,46 @@ package { "unzip":
   ensure => installed
 }
 
+file { "/etc/participle":
+  ensure => directory,
+  owner => 'measurements-api',
+  group => 'measurements-api'
+}
+
+file { "/etc/participle/measurement-api/":
+  ensure => directory,
+  owner => 'measurements-api',
+  group => 'measurements-api',
+  require => [
+        File['/etc/participle/']
+  ]
+}
+
+file { "/etc/participle/measurement-api/api-keys.properties":
+  content => template('measurements-api/api-keys.properties.erb'),
+  mode   => '0400',
+  owner => 'measurements-api',
+  group => 'measurements-api',
+  require => [
+        File['/etc/participle/measurement-api/']
+  ]
+}
+
 upstart::job { 'measurements-api':
   description    => "Capability Measurements API",
   user           => 'measurements-api',
   group          => 'measurements-api',
   chdir          => '/home/measurements-api/measurements-api',
+  environment    => {
+     'MEASUREMENTS_API_KEYS_LOCATION' => '/etc/participle/measurement-api/api-keys.properties'
+  },
   service_enable => false,
   service_ensure => 'stopped',
   exec           => 'sh /home/measurements-api/measurements-api/bin/measurements-api',
   require        => [
                       File['/home/measurements-api'],
                       File['/home/measurements-api/.ssh/authorized_keys'],
+                      File['/etc/participle/measurement-api/api-keys.properties'],
                       Package['nginx']
                     ]
 }
